d2a8cb54c83d630a15712d7e0da69d4ecf4e4e04
levircesar <levirlemos@gmail.com>
2021-02-25 10:25:39 -0300

    aula 3 concluida

--- src/components/CompletedChallengers.tsx
+++ src/components/CompletedChallengers.tsx
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompletedChallengers.module.css';
 
 export function CompletedChallengers(){
+
+    const{ challengesCompleted } = useContext(ChallengesContext);
+    
     return(
         <div className={styles.completedChallengersContainer}>
             <span>Desafios Completos</span>
-            <span>5</span>
+            <span>{ challengesCompleted }</span>
         </div>
     );
 }
---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,87 @@
+import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge{
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
+
+interface ChallengesContextData{
+    level:number;
+    currentExperience:number;
+    challengesCompleted:number;
+    levelUp:()=>void;   
+    startNewChallenge:()=>void;
+    activeChallenge: Challenge;
+    resetChallenge :()=>void;
+    experienceTonextLevel:number;
+    completeChallenge:() =>void;
+}
+
+interface ChallengesProviderProps{
+    children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({ children }:ChallengesProviderProps){
+
+    const [level, setLevel] = useState(1);
+    const [currentExperience , setCurrentExperience] = useState(0);
+    const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
+    const experienceTonextLevel = Math.pow((level + 1) * 4,2)
+
+    function levelUp(){
+        setLevel(level + 1);
+    }
+
+    function startNewChallenge(){
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+        setActiveChallenge(challenge);
+    }
+
+    function resetChallenge(){
+        setActiveChallenge(null);
+    }
+
+    function completeChallenge(){
+        if(!activeChallenge){
+            return ;
+        }
+
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if(finalExperience >= experienceTonextLevel){
+            finalExperience = finalExperience - experienceTonextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+
+    return(
+        <ChallengesContext.Provider 
+        value={{ 
+                level,
+                currentExperience,
+                challengesCompleted, 
+                levelUp,
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge,
+                experienceTonextLevel,
+                completeChallenge
+            }}>
+            {children}
+        </ChallengesContext.Provider>
+    );
+}
